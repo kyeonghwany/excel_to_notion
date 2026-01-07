d90636552eb4f2f744eb90cd8880096f1acac77c
kyeonghwany <james160601@gmail.com>
2026-01-07 16:13:14 +0900

    fix

--- preprocess.py
+++ preprocess.py
@@ -34,8 +34,8 @@ def preprocess_reservation(df: pd.DataFrame) -> pd.DataFrame:  # noqa: F811
     df["상태"] = df["상태"].map(state_code)
     df["핸드폰"] = (
         df["핸드폰"]
-        .astype("string"))
-    df["생년월일"] = (
+        .astype("string").map(lambda x: "" if pd.isna(x) else str(x)))
+    df["생년월일"] = (+
         df["생년월일"]
         .astype("string"))
 
